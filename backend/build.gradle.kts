2933e987a23415ea81039eb27da1bbf382a62e8f
Ole Birketvedt <fossbab@gmail.com>
2025-11-27 19:48:29 +0100

    Revert Spring dependencies to pre-4.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     // implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.data:spring-data-jpa:4.0.0")
+    implementation("org.springframework.data:spring-data-jpa:3.5.5")
     runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -31,15 +31,15 @@ dependencies {
     implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
     implementation("com.h2database:h2:2.4.240")
     // https://mvnrepository.com/artifact/org.springframework/spring-tx
-    implementation("org.springframework:spring-tx:7.0.1")
-    implementation("org.springframework:spring-orm:7.0.1")
+    implementation("org.springframework:spring-tx:6.2.12")
+    implementation("org.springframework:spring-orm:6.2.12")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
 
     // https://mvnrepository.com/artifact/io.valkey/valkey-java
     implementation("io.valkey:valkey-java:5.5.0")
 
     // https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.14")
 
     implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
